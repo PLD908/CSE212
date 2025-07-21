4d3b6cefad772be0d381b9881646028c27bf3735
PLD908 <oluyededivine@gmail.com>
2025-07-21 14:06:08 +0100

    W03: Implemented Basketball points summary with map and top 10

--- week03/teach/Basketball.cs
+++ week03/teach/Basketball.cs
@@ -23,14 +23,28 @@ public class Basketball
         reader.TextFieldType = FieldType.Delimited;
         reader.SetDelimiters(",");
         reader.ReadFields(); // ignore header row
-        while (!reader.EndOfData) {
+        while (!reader.EndOfData)
+        {
             var fields = reader.ReadFields()!;
             var playerId = fields[0];
             var points = int.Parse(fields[8]);
+            if (players.ContainsKey(playerId))
+                players[playerId] += points;
+            else
+                players[playerId] = points;
         }
 
-        Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
+        // Convert to array and sort by points descending
+        var topPlayers = players.ToArray();
+        Array.Sort(topPlayers, (p1, p2) => p2.Value - p1.Value);
 
-        var topPlayers = new string[10];
+        Console.WriteLine($"Players: {{{string.Join(", ", players.Select(p => $"{p.Key}: {p.Value}"))}}}");
+
+        // Display top 10 players
+        Console.WriteLine("\nTop 10 Players by Total Points:");
+        for (var i = 0; i < Math.Min(10, topPlayers.Length); ++i)
+        {
+            Console.WriteLine($"{topPlayers[i].Key}: {topPlayers[i].Value}");
+        }
     }
 }
--- week03/teach/DisplaySums.cs
+++ week03/teach/DisplaySums.cs
@@ -1,5 +1,10 @@
-﻿public static class DisplaySums {
-    public static void Run() {
+﻿using System;
+using System.Collections.Generic;
+
+public static class DisplaySums
+{
+    public static void Run()
+    {
         DisplaySumPairs([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
         // Should show something like (order does not matter):
         // 6 4
@@ -27,7 +32,22 @@
     /// in the list.
     /// </summary>
     /// <param name="numbers">array of integers</param>
-    private static void DisplaySumPairs(int[] numbers) {
-        // TODO Problem 2 - This should print pairs of numbers in the given array
+    private static void DisplaySumPairs(int[] numbers)
+    {
+        HashSet<int> seenNumbers = new HashSet<int>();
+
+        foreach (int num in numbers)
+        {
+            int complement = 10 - num;
+            if (seenNumbers.Contains(complement))
+            {
+                // Print the pair with the smaller number first to avoid duplicates
+                if (num < complement)
+                    Console.WriteLine($"{num} {complement}");
+                else
+                    Console.WriteLine($"{complement} {num}");
+            }
+            seenNumbers.Add(num);
+        }
     }
 }
